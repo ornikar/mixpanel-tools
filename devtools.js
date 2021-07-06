c0be9c3ace20a947121dc13e0aab22f5a44ea678
Christophe Hurpeau <christophe@hurpeau.com>
2021-07-06 18:22:42 +0200

    Rename extension

--- devtools.js
+++ devtools.js
@@ -1,5 +1,5 @@
 
 // Create a tab in the devtools area
-chrome.devtools.panels.create("Mixpanel Tools", "logo.png", "panel.html", function (panel) { 
+chrome.devtools.panels.create("Ornikar Mixpanel Tools", "logo.png", "panel.html", function (panel) { 
     console.log(panel);
 });
